f13e83f8f740c16489b7a2f5765e6236e29e2e73
anphongdoa5 <anphongdoa5@gmail.com>
2026-04-08 16:10:59 +0700

    cau 2

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenRouterController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\MovieController2;
 
 Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
-
-
-
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
+Route::get('/the-loai/{id}', [App\Http\Controllers\MovieController::class, 'getMoviesByGenre']);
+Route::get('/movie/{id}', [App\Http\Controllers\MovieController::class, 'detail'])->name('movie.detail');
+
+
+
+Route::get('/movies/{id}', [MovieController2::class, 'show']);
+Route::delete('/movies/{id}', [MovieController2::class, 'destroy']);
+Route::post('/timkiem', [MovieController::class, 'search']);
+
